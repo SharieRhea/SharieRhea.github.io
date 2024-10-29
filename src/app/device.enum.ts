e2707cc36de7833543d9d70f035d3bfa86bf8b4d
SharieRhea <sharierhea@gmail.com>
2024-10-29 13:11:01 -0400

    feat: add enum for device type to start improving responsiveness

---
+++ src/app/device.enum.ts
@@ -0,0 +1,5 @@
+export enum Device {
+  Phone = "phone",
+  Tablet = "tablet",
+  Default = "default"
+}
--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,26 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { SocialLinkButtonComponent } from './social-link-button.component';
 import { ButtonModule } from 'primeng/button';
 import { NavigationBarComponent } from './navigation-bar.component';
 import { ProjectShowcaseComponent } from './project-showcase.component';
+import { Device } from './device.enum';
 
 @Component({
   selector: 'home-component',
   standalone: true,
-  imports: [SocialLinkButtonComponent, RouterOutlet, RouterLink, RouterLinkActive, ButtonModule, NavigationBarComponent, ProjectShowcaseComponent],
+  imports: [CommonModule, SocialLinkButtonComponent, RouterOutlet, RouterLink, RouterLinkActive, ButtonModule, NavigationBarComponent, ProjectShowcaseComponent],
   template: `
     <navigation-bar-component/>
-    <h2>Hi there, I'm</h2>
-    <h1>SHARIE RHEA</h1>
-    <p class="subtitle">A computer science student, driven learner, and jack-of-all-trades.</p>
-    <div id="social-links">
+    <h2 [ngClass]="device">Hi there, I'm</h2>
+    <h1 [ngClass]="device">SHARIE RHEA</h1>
+    <p class="subtitle" *ngIf="device == deviceEnum.Default">A computer science student, driven learner, and jack-of-all-trades.</p>
+    <div id="social-links" *ngIf="device == deviceEnum.Default">
       <social-link-button-component label="GitHub" link="https://github.com/SharieRhea"/>
       <social-link-button-component label="LinkedIn" link="https://www.linkedin.com/in/sharierhea"/>
     </div>
-    <project-showcase-component />
+    <project-showcase-component *ngIf="device == deviceEnum.Default"/>
   `,
   styles: `
     :host {
@@ -45,6 +48,22 @@ import { ProjectShowcaseComponent } from './project-showcase.component';
     }
   `
 })
-export class HomeComponent {
-  title = 'sharierhea.dev';
+export class HomeComponent implements OnInit {
+  deviceEnum = Device;
+  device = Device.Default;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver.observe([Breakpoints.Handset, Breakpoints.Tablet]).subscribe(result => {
+      const breakpoints = result.breakpoints;
+
+      if (breakpoints[Breakpoints.HandsetPortrait])
+        this.device = Device.Phone;
+      else if (breakpoints[Breakpoints.TabletPortrait])
+        this.device = Device.Tablet;
+      else
+        this.device = Device.Default;
+    });
+  }
 }
