54fe664bc9e92e9a441754bf496e73c32bc3fe0c
SharieRhea <sharierhea@gmail.com>
2025-02-17 18:32:03 -0500

    feat: add quasher test page with qr codes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,12 @@ import { Home } from './home/home.component';
 import { ProjectShowcase } from './project/project-showcase/project-showcase.component';
 import { Projects } from './project/projects.component';
 import { About } from './about/about.component';
+import { QrCodes } from './qr/qrcodes.component';
 
 export const routes: Routes = [
+  // test pages for quasher
+  { path: "qr", component: QrCodes},
+
   { path: "projects/:projectKey", component: ProjectShowcase },
   { path: "about", component: About },
   { path: "projects", component: Projects },
---
+++ src/app/qr/qrcodes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'qr-codes',
+  standalone: true,
+  templateUrl: './qrcodes.component.html',
+  styleUrls: ['./qrcodes.component.css'],
+  imports: [],
+})
+export class QrCodes { }
