3776ed56d75076767b90f56807823b3a3e6fc2c2
SharieRhea <sharierhea@gmail.com>
2025-11-01 12:09:02 -0400

    feat: update faq, fix wildcard routing, add gitignore

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,14 @@ import { Home } from './home/home.component';
 import { ProjectShowcase } from './project/project-showcase/project-showcase.component';
 import { Projects } from './project/projects.component';
 import { About } from './about/about.component';
-import { QrCodes } from './qr/qrcodes.component';
 import { Faq } from './faq/faq.component';
 
 export const routes: Routes = [
-  // test pages for quasher
-  { path: "qr", component: QrCodes},
-
   { path: "faq", component: Faq },
   { path: "projects/:projectKey", component: ProjectShowcase },
   { path: "about", component: About },
   { path: "projects", component: Projects },
   { path: "home", component: Home },
-  { path: "", redirectTo: "/home", pathMatch: "full" }
+  // redirect any unknown path to home
+  { path: "**", redirectTo: "/home", pathMatch: "full" }
 ];
